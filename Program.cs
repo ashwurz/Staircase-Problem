cb0eef0c13db1801fa8c0155c926a29b6341de42
Helmuth Ausgut Risch Filho <helmuthfilho001@gmail.com>
2018-06-08 11:27:41 -0300

    Staircase problem

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void espacos(int contagem, int n)
+        {
+            for (int i = 0; i < n - contagem; i++)
+            {
+                Console.Write(" ");
+            }
+        }       
+        static void rashtag(int contagem)
+        {
+            for (int i = 0; i < contagem; i++)
+            {
+                Console.Write("#");
+            }
+        }
+        static void staircase(int n)
+        {
+            int nHash = 0;
+            for (int i = 0; i < n; i++)
+            {
+                nHash = i + 1;
+                espacos(nHash, n);
+                rashtag(nHash);
+                Console.Write("\n");
+            }
+        }          
+        static void Main(string[] args)
+        {
+            int n = 6;
+            staircase(n);
+        }
+    }
+}
